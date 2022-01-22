2806cef4641b8757090a38c0cc90f348d7cbe008
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2022-01-22 17:25:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var preResult = "";
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function modelLoaded(){
+  console.log("MoblieNET Model Is Loaded");
+}
+
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
+
+function gotResult(error,results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    if((results[0].confidence > 0.5) && (preResult != results[0].label )){
+      console.log("result is - "+results[0].label);
+      preResult = results[0].label;
+      var syth = window.speechSynthesis;
+      var speech_data = 'object detected is ' + results[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speech_data);
+      syth.speak(utterThis);
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML =  results[0].confidence.toFixed(3);
+    }
+  }
+}
